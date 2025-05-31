6796c1b00e1f33620265ee43dd8428fc77a21ff6
Vishwanathdnaik <157128997+Vishwanathdnaik@users.noreply.github.com>
2025-06-01 04:33:35 +0530

    Add files via upload

---
+++ .eleventy.js.js
@@ -0,0 +1,12 @@
+
+const kickstartDS = require("@kickstartds/eleventy-plugin-kickstartds");
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(kickstartDS);
+  return {
+    dir: {
+      input: "src",
+      output: "dist"
+    }
+  };
+};
