de80974400a78ba76b2aeaad566b006aec7a822a
Vishwanathdnaik <157128997+Vishwanathdnaik@users.noreply.github.com>
2025-06-01 04:38:49 +0530

    Add files via upload

--- .eleventy.js
+++ .eleventy.js
@@ -1,18 +1,12 @@
-const kdsPlugin = require("@kickstartds/eleventy-plugin-kickstartds");
-const pluginRss = require("@11ty/eleventy-plugin-rss");
+
+const kickstartDS = require("@kickstartds/eleventy-plugin-kickstartds");
 
 module.exports = function (eleventyConfig) {
-  eleventyConfig.addPlugin(kdsPlugin);
-  eleventyConfig.addPlugin(pluginRss);
-
-  eleventyConfig.addPassthroughCopy({
-    static: "static",
-    "static/img": "img",
-    "node_modules/@kickstartds/ds-agency/dist/static/favicon": "/",
-  });
+  eleventyConfig.addPlugin(kickstartDS);
   return {
     dir: {
       input: "src",
-    },
+      output: "dist"
+    }
   };
 };
